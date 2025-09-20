440f14d8fc0f18e44252ca932b9210713eaca9db
mudrecodmkd <95343872+mudrecodmkd@users.noreply.github.com>
2025-09-21 00:18:07 +0200

    Update chat_store.dart

--- lib/storage/chat_store.dart
+++ lib/storage/chat_store.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/message.dart';
+import '../models/message.dart';
 
 class ChatStore {
   static const _key = 'chat_history_v3';
